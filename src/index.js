27308cecd6a1647e7a94a2eadf5746cbb52ffe17
Alex Garza <preemo956@gmail.com>
2023-11-23 20:41:25 -0600

    added basic model

--- src/index.js
+++ src/index.js
@@ -4,18 +4,20 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from "cors"
 import helmet from 'helmet'
+import bodyParser from 'body-parser'
 
 import logger from './utils/logger'
 import router from './routes'
 import { notFound, errorHandler } from './utils/errors'
 
-const port = parseInt(process.env.PORT, 10) || 4000
+const port = Number(process.env.PORT, 10) || 4000
 
 const app = express()
 
 app.use(morgan(process.env.MORGAN_LOG))
 app.use(cors({ origin: process.env.CORS_ORIGIN }))
 app.use(helmet())
+app.use(bodyParser.json())
 
 app.use(router)
 
