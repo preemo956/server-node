c67e01b85be477adab3f96efc7f2ff333b07545c
nhinojosa <nhinojosa@southtexascollege.edu>
2023-11-30 21:43:02 -0600

    code committed out

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,8 +2,8 @@ import { Router } from "express"
 import basicAuth from 'express-basic-auth'
 
 import persons from "./persons"
-// import companies from "./companies"
-// import departments from "./departments"
+import companies from "./companies"
+//import departments from "./departments"
 // import employees from "./employees"
 const router = Router()
 
@@ -19,7 +19,7 @@ router.get('/', (req, res) => {
 })
 
 router.use('/persons', persons)
-// router.use('/companies', companies)
+router.use('/companies', companies)
 // router.use('/departments', departments)
 // router.use('/employees', employees)
 export default router
